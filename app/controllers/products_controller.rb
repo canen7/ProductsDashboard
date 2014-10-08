c8c48caa3831a1ff3a4bd782b0edd48f54caa349
Alvaro Canencia <Alvaro@Alvaros-MacBook-Pro.local>
2014-10-08 15:23:26 -0700

    first commit of products with categories added

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,73 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+
+  end
+
+  def new
+    @categories = Category.all
+    @product = Product.new
+  end
+
+  def edit
+    @categories = Category.all
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    # this is the equivalent of a var dumpo for my form data
+    #render :text => params.inspect
+    @product = Product.new(product_params)
+    # attempt to save pug to database
+    if @product.save
+      flash[:success] = 'New product succesfully created'
+      redirect_to products_path
+    else
+      @errors = @product.errors.full_messages
+      @categories = Category.all
+      render :new
+    end
+  end
+
+  def update
+   #raise
+    @product = Product.find(params[:id])
+    # render :text => params.inspect
+    if @product.update(product_params)
+      flash[:message] = "product updated succesfully"
+      redirect_to product_path(@product)
+    else
+      @categories = Category.all
+      render :edit
+    end
+  end
+
+  def destroy
+      @product = Product.find(params[:id])
+    if @product.delete
+      flash[:message] = "Product DELETED succesfully"
+      redirect_to products_path
+    else
+      flash[:message] = "Product CANNOT be deleted"
+      redirect_to products_path
+    end
+  end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :pricing, :category_id)
+  end
+
+  def set_product
+      #this runs every time we need it to automatically before the action
+      @product= Product.find(params[:id])
+  end
+
+end
